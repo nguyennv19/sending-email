6901423dfda6fcb2e1c39c57e46f38f3f5358dba
Nguyen Nguyen <nvnguyen19@gmail.com>
2022-12-07 14:58:46 +0700

    Added Github action workflow in the send-out email

--- send-email.py
+++ send-email.py
@@ -9,12 +9,16 @@ smtp_server = "smtp.gmail.com"
 USER_EMAIL = os.environ.get("NOTI_USERNAME")
 USER_PASSWORD = os.environ.get("NOTI_PASS")
 DESTINATION_EMAIL = os.environ.get("DESTINATION_EMAIL")
+GITHUB_RUNID = os.environ.get("GITHUB_RUNID")
+GITHUB_REPO = os.environ.get("GITHUB_REPO")
 
 ##### EMAIL INFO
 from_addr = USER_EMAIL
 to_addr = DESTINATION_EMAIL #"dev-ops@whydah.xyz"
 subj = "Failed email checking"
-body = "Dear sir,\nEmail checking rule has been violated!"
+repo = GITHUB_REPO
+ref = f"https://github.com/{GITHUB_REPO}/actions/runs/{GITHUB_RUNID}"
+body = f"Dear sir,\nEmail checking rule has been violated!\nPlease check it out at: {ref}"
 msg = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (from_addr, to_addr, subj, body)
 
 
